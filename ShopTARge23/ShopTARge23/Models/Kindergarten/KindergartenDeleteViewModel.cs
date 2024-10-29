7909a05de10d1ab4c130c65f22855d2c3def3bd8
Opilane TTHK <opilane@tthk.ee>
2024-10-29 13:05:46 +0200

    kindergartenErrors

---
+++ ShopTARge23/ShopTARge23/Models/Kindergarten/KindergartenDeleteViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ShopTARge23.Core.Dto;
+using ShopTARge23.Models.Kindergarten;
+
+namespace ShopTARge23.Models.Kindergarten
+{
+    public class KindergartenDeleteViewModel
+    {
+        public Guid? Id { get; set; }
+        public string GroupName { get; set; }
+        public int ChildrenCount { get; set; }
+        public string KindergartenName { get; set; }
+        public string Teacher { get; set; }
+        public List<KindergartenImageViewModel> Image { get; set; }
+            = new List<KindergartenImageViewModel>();
+
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
