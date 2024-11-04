6e84109025db34c9b5bbe879bf616b264a5c0131
Opilane TTHK <opilane@tthk.ee>
2024-11-04 11:31:32 +0200

    weather

---
+++ ShopTARge23/ShopTARge23.ApplicationServices/Services/WeatherForecastServices.cs
@@ -0,0 +1,25 @@
+﻿using Nancy.Json;
+using ShopTARge23.Core.Dto.WeatherDtos.AccuWeatherDtos;
+using System.Net;
+
+
+namespace ShopTARge23.ApplicationServices.Services
+{
+    public class WeatherForecastServices
+    {
+        public async Task<AccuLocationWeatherResultDto> AccuWeatherResult(AccuLocationWeatherResultDto dto)
+        {
+            string accuApiKey = "QDALiSm2r5SpVY5Bm41qLzisj1puDHwQ";
+            string url = $"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={accuApiKey}&q={dto.CityName}";
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+                AccuLocationRootDto accuResult = new JavaScriptSerializer().Deserialize<AccuLocationRootDto>(json);
+            }
+
+            return dto;
+        }
+    }
+}
---
+++ ShopTARge23/ShopTARge23.Core/Dto/WeatherDtos/AccuWeatherDtos/AccuLocationWeatherResultDto.cs
@@ -0,0 +1,26 @@
+﻿
+using Castle.Components.DictionaryAdapter;
+
+namespace ShopTARge23.Core.Dto.WeatherDtos.AccuWeatherDtos
+{
+    public class AccuLocationWeatherResultDto
+    {
+        public string CityName { get; set; }
+        public string CityCode { get; set; }
+        public int Rank { get; set; }
+
+
+        public Int32 Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public string LocalizedName { get; set; }
+        public string EnglishName { get; set; }
+        public string PrimaryPostalCode { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string AdministrativeArea { get; set; }
+        public string TimeZone { get; set; }
+
+
+    }
+}
